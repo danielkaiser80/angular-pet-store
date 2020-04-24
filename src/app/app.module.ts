40535d6288823ab13f8e2ee8709a59a28fb7916d
Daniel Kaiser <d.kaiser@wealthpilot.de>
2020-04-24 10:55:21 +0200

    Add better header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,25 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {InventoryComponent} from './inventory/inventory.component';
 import {HeaderComponent} from './header/header.component';
-import {RouterModule} from "@angular/router";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {NgxChartsModule} from "@swimlane/ngx-charts";
 import {FormsModule} from "@angular/forms";
 import {MatButtonModule} from '@angular/material/button';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatMenuModule} from '@angular/material/menu';
-import { RoutingModule } from './routing/routing.module';
+import {RoutingModule} from './routing/routing.module';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 
 @NgModule({
   exports: [
     MatButtonModule,
     MatButtonToggleModule,
-    MatMenuModule
+    MatMenuModule,
+    MatToolbarModule,
+    FlexLayoutModule
   ],
   declarations: [
     AppComponent,
@@ -32,6 +36,7 @@ import { RoutingModule } from './routing/routing.module';
     NgxChartsModule,
     BrowserAnimationsModule,
     RoutingModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,4 +12,7 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onToggleNav() {
+
+  }
 }
