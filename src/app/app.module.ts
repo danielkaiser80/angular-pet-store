de04dd6e45797b2c9ad68d2520b69342967746b5
Daniel Kaiser <d.kaiser@wealthpilot.de>
2020-04-24 10:36:29 +0200

    Add material style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,17 @@ import {RouterModule} from "@angular/router";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {NgxChartsModule} from "@swimlane/ngx-charts";
 import {FormsModule} from "@angular/forms";
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatMenuModule} from '@angular/material/menu';
+
 
 @NgModule({
+  exports: [
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatMenuModule
+  ],
   declarations: [
     AppComponent,
     InventoryComponent,
